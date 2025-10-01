ed546c5f6bb9d95a4a6d017b68093dcf53e7ea58
Placeboguy <ispks2005@gmail.com>
2025-10-01 16:08:29 +0530

    server.js updated for vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   }
 
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] 
+    <div className="bg-[url('/bgImage.svg')] 
     bg-contain">
       <Toaster />
       <Routes>
